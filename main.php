231a79105aad4a0f3ce836425fca0ebe21d1a109
MGShio <maxenceguenaou@yahoo.ca>
2026-04-09 13:55:48 +0200

    Command + main.php
    Ajout des commandes :
    -Help
    -Delete
    -Create
    -Detail

--- main.php
+++ main.php
@@ -15,12 +15,22 @@ while (true) {
 
     if (trim(strtolower($line)) === "list") {
         Command::list();
-    }
-    elseif (trim(strtolower($line)) === "exit") {
+    } elseif (preg_match('/^detail \d+$/', $line)) {
+        Command::detail($line);
+        // Commande sur le terminal : detail 1
+    } elseif (preg_match('/^create .+, .+, .+$/', $line)) {
+        Command::create($line);
+        // Commande sur le terminal : create John Doe, john.doe@example.com, 1234567890
+    } elseif (preg_match('/^delete \d+$/', $line)) {
+        Command::delete($line);
+        // Commande sur le terminal : delete 1
+    } elseif (trim(strtolower($line)) === "help") {
+        Command::help();
+        // Commande sur le terminal : help
+    } elseif (trim(strtolower($line)) === "exit") {
         echo "Au revoir!\n";
         break;
-    }
-    else {
+    } else {
         echo "Commande inconnue : $line\n";
     }
 }
